64152b0c86fce63c4942be9be7f33a6cc4690a79
Daniel Krol <orblivion@gmail.com>
2011-08-02 09:31:04 -0500

    Makes get_transparent_redirect_form_initial

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+from django.conf import settings 
+from django.core.urlresolvers import reverse
 
 def new_payment_method_redirect(request):
     new_method = PaymentMethod(payment_method_token = request.GET['payment_method_token'])
@@ -22,3 +24,6 @@ def update_payment_method_redirect(request):
     except ValidationError: # not attached to the right user
         return get_view_by_string(settings.FEEFIGHTER_UPDATEMETHOD_ERROR_REDIRECT)
 
+
+def get_transparent_redirect_form_initial(base_url): 
+    return {'merchant_key': settings.FEEFIGHTERS_CREDENTIALS.merchant_key, 'redirect_url': base_url + reverse('samurai-redirect')}

d182dea5a867a844af7d3396ff2ba8ccf5d8aa80
Joshua Krall <joshuakrall@pobox.com>
2011-10-07 16:36:24 -0500

    Edited test_credentials.py via GitHub

--- test_credentials.py
+++ test_credentials.py
@@ -1,14 +1,5 @@
 # These are credentials which are necessesary to run the test suite.
 # You should not put credentials for real merchants or processors here!
-
-# To use this, put in your test credentials, and copy this file to test_credentials.py
-
-# Samurai Sandbox Test
-# processor_token = "0c7eeeb8cc23a79f381bc886"
-# merchant_key = "e84803ad9cf61cb21405ab79"
-# merchant_password = "a368e3adcd41a4ed29e7bc3c"
-
-# Edulender Auth.net Sandbox Test
 processor_token = "69ac9c704329bb067d427bf0"
 merchant_key = "a1ebafb6da5238fb8a3ac9f6"
 merchant_password = "ae1aa640f6b735c4730fbb56"

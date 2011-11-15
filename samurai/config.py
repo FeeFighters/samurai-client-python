dcdde3933b8b45ad107da2347e153cfbb6053efa
Rahul Kumar <rahulsinner@gmail.com>
2011-11-16 05:28:37 +0530

    Checking in dev keys for testing.

--- samurai/config.py
+++ samurai/config.py
@@ -4,5 +4,6 @@ Configs for Samurai API.
 
 Set samurai api configurations on this module.
 """
-merchant_key = ''
-merchant_password = ''
+merchant_key = 'a1ebafb6da5238fb8a3ac9f6'
+merchant_password = 'ae1aa640f6b735c4730fbb56'
+top_uri='https://api.samurai.feefighters.com/v1/',

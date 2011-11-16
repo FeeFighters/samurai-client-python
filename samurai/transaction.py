8cf0e8ca99ded9edee1248fd3dc8d1e66567dc07
Rahul Kumar <rahulsinner@gmail.com>
2011-11-17 03:51:43 +0530

    Transaction specific check_for_errors.

--- samurai/transaction.py
+++ samurai/transaction.py
@@ -26,3 +26,24 @@ class Transaction(ApiBase):
         """
         super(ApiBase, self).__init__()
         self.update_fields(xml_res)
+
+    def message_block(self, parsed_res):
+        """
+        Returns the message block from the `parsed_res`
+        """
+        return (parsed_res.get(self.top_xml_key) and
+                parsed_res[self.top_xml_key].get('processor_response') and
+                parsed_res[self.top_xml_key]['processor_response'].get('messages'))
+
+    def check_for_errors(self, parsed_res):
+        """
+        Checks `parsed_res` for error blocks.
+        If the transaction failed, sets `self.errors`
+        Else delegates to superclass.
+        """
+        if not parsed_res[self.top_xml_key]['processor_response']['success']:
+            message_block = self.message_block(parsed_res)
+            if message_block and message_block.get('message'):
+                self.errors = message_block['message']
+            return True
+        return super(ApiBase, self).check_for_errors(parsed_res)

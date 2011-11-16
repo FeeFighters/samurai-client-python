402a85cf891c7b6a68d4e3248487b499c3b5ad01
Rahul Kumar <rahulsinner@gmail.com>
2011-11-17 03:51:20 +0530

    Adding message block and renaming check_for_errors.

--- samurai/api_base.py
+++ samurai/api_base.py
@@ -17,7 +17,13 @@ class ApiBase(object):
     def __init__(self):
         self.errors = None
 
-    def check_for_error(self, parsed_res):
+    def message_block(self, parsed_res):
+        """
+        Returns the message block from the `parsed_res`
+        """
+        return parsed_res.get(self.top_xml_key) and parsed_res[self.top_xml_key].get('messages')
+
+    def check_for_errors(self, parsed_res):
         """
         Checks `parsed_res` for error blocks.
         If it contains error blocks, it return True and sets errors.
@@ -27,14 +33,16 @@ class ApiBase(object):
         # Check high level errors.
         if parsed_res.get('error'):
             error = True
-            if parsed_res['error'].get('messages'):
-                self.errors = parsed_res['error']['messages'].get
+            if parsed_res['error'].get('messages') and parsed_res['error']['messages'].get('message'):
+                self.errors = parsed_res['error']['messages']['message']
         # Check request specific error.
-        elif parsed_res.get(self.message_block) and parsed_res[self.message_block].get('message'):
-            error = any(True for m in parsed_res[self.message_block]['message']
-                        if m['class'] == 'error')
-            if error:
-                self.errors = parsed_res[self.message_block]
+        else:
+            message_block = self.message_block(parsed_res)
+            if message_block and message_block.get('message'):
+                error = any(True for m in message_block['message']
+                            if m['class'] == 'error')
+                if error:
+                    self.errors = message_block['message']
         return error
 
     def update_fields(self, xml_res):
@@ -42,6 +50,6 @@ class ApiBase(object):
         Updates field with the returned `xml_res`.
         """
         parsed_res = xml_to_dict(xml_res)
-        if not self.check_for_error(parsed_res) and parsed_res.get(self.top_xml_key):
+        if not self.check_for_errors(parsed_res) and parsed_res.get(self.top_xml_key):
             self.__dict__.update(**parsed_res[self.top_xml_key])
 

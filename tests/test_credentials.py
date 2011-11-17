a047aa90d493c52b081972bd1fb0873a70a0102d
Rahul Kumar <rahulsinner@gmail.com>
2011-11-18 04:57:04 +0530

    Adding payment method test.

--- tests/test_credentials.py
+++
@@ -1,6 +0,0 @@
-# These are credentials which are necessesary to run the test suite.
-# You should not put credentials for real merchants or processors here!
-processor_token = "5a0e1ca1e5a11a2997bbf912"
-merchant_key = "a1ebafb6da5238fb8a3ac9f6"
-merchant_password = "ae1aa640f6b735c4730fbb56"
-
---
+++ tests/test_helper.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('..')
+
+import samurai.config as config
+
+config.merchant_key = 'a1ebafb6da5238fb8a3ac9f6'
+config.merchant_password = 'ae1aa640f6b735c4730fbb56'

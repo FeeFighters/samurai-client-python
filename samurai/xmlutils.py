2d5faa14198452787037f23e4fba55a19857e10b
Rahul Kumar <rahulsinner@gmail.com>
2011-11-19 01:48:41 +0530

    Prefixing implementation details with a _

--- samurai/xmlutils.py
+++ samurai/xmlutils.py
@@ -14,36 +14,36 @@ def xml_to_dict(root_or_str):
     if isinstance(root, str):
         import xml.etree.cElementTree as ElementTree
         root = ElementTree.XML(root_or_str)
-    return {root.tag: from_xml(root)}
+    return {root.tag: _from_xml(root)}
 
 def dict_to_xml(dict_xml):
     """
     Converts `dict_xml` which is a python dict to corresponding xml.
     """
-    return xml_from_dict(dict_xml)
+    return _xml_from_dict(dict_xml)
 
 # Functions below this line are implementation details.
 # Unless you are changing code, don't bother reading.
 # The functions above constitute the user interface.
 
-def to_xml(tag, content):
+def _to_xml(tag, content):
     if isinstance(content, dict):
         val = '<%(tag)s>%(content)s</%(tag)s>' % dict(tag=tag,
-                                                      content=xml_from_dict(content))
+                                                      content=_xml_from_dict(content))
     else:
         val = '<%(tag)s>%(content)s</%(tag)s>' % dict(tag=tag, content=content)
     return val
 
-def xml_from_dict(dict_xml):
+def _xml_from_dict(dict_xml):
     """
     Converts `dict_xml` which is a python dict to corresponding xml.
     """
     tags = []
     for tag, content in dict_xml.iteritems():
-        tags.append(to_xml(tag, content))
+        tags.append(_to_xml(tag, content))
     return ''.join(tags)
 
-def is_xml_el_dict(el):
+def _is_xml_el_dict(el):
     """
     Returns true if `el` is supposed to be a dict.
     This function makes sense only in the context of making dicts out of xml.
@@ -52,7 +52,7 @@ def is_xml_el_dict(el):
         return True
     return False
 
-def is_xml_el_list(el):
+def _is_xml_el_list(el):
     """
     Returns true if `el` is supposed to be a list.
     This function makes sense only in the context of making lists out of xml.
@@ -61,61 +61,61 @@ def is_xml_el_list(el):
         return True
     return False
 
-def from_xml(el):
+def _from_xml(el):
     """
     Extracts value of xml element element `el`.
     """
     val = None
     # Parent node.
     if el:
-        if is_xml_el_dict(el):
-            val = dict_from_xml(el)
-        elif is_xml_el_list(el):
-            val = list_from_xml(el)
+        if _is_xml_el_dict(el):
+            val = _dict_from_xml(el)
+        elif _is_xml_el_list(el):
+            val = _list_from_xml(el)
     # Simple node.
     else:
         attribs = el.items()
         # An element with no subelements but text.
         if el.text:
-            val = val_and_maybe_convert(el)
+            val = _val_and_maybe_convert(el)
         # An element with attributes.
         elif attribs:
             val = dict(attribs)
     return val
 
-def val_and_maybe_convert(el):
+def _val_and_maybe_convert(el):
     """
     Converts `el.text` if `el` has attribute `type` with valid value.
     """
     text = el.text.strip()
     data_type = el.get('type')
-    convertor = val_and_maybe_convert.convertors.get(data_type)
+    convertor = _val_and_maybe_convert.convertors.get(data_type)
     if convertor:
         return convertor(text)
     else:
         return text
-val_and_maybe_convert.convertors = {
+_val_and_maybe_convert.convertors = {
     'boolean': str_to_boolean,
     'datetime': str_to_datetime,
     'integer': int
 }
 
-def list_from_xml(els):
+def _list_from_xml(els):
     """
     Converts xml elements list `el_list` to a python list.
     """
     res = []
     tag = els[0].tag
     for el in els:
-        res.append(from_xml(el))
+        res.append(_from_xml(el))
     return {tag: res}
 
-def dict_from_xml(els):
+def _dict_from_xml(els):
     """
     Converts xml doc with root `root` to a python dict.
     """
     # An element with subelements.
     res = {}
     for el in els:
-        res[el.tag] = from_xml(el)
+        res[el.tag] = _from_xml(el)
     return res

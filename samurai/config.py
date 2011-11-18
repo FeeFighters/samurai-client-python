d48cddb1c54f5ee9b9fa0e4902b4de5e3b779877
Rahul Kumar <rahulsinner@gmail.com>
2011-11-19 03:22:59 +0530

    Adding usage documentation to config module.

--- samurai/config.py
+++ samurai/config.py
@@ -3,7 +3,14 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~
 
     Set samurai api configurations on this module.
+
+    Import this module and set the `merchant_key` and `merchant_password`.
+    Other modules use the configuration set on this object.
+    ::
+        import samurai.config as config
+        config.merchant_key = your_merchant_key
+        config.merchant_password = your_merchant_password
 """
-merchant_key = 'a1ebafb6da5238fb8a3ac9f6'
-merchant_password = 'ae1aa640f6b735c4730fbb56'
+merchant_key = None
+merchant_password = None
 top_uri='https://api.samurai.feefighters.com/v1/',

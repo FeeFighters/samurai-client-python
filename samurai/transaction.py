d8f9325f544d6504b05e41ee26c7f7623210de43
Rahul Kumar <rahulsinner@gmail.com>
2011-11-19 03:13:36 +0530

    Adding is_success and is_declined transaction methods.

--- samurai/transaction.py
+++ samurai/transaction.py
@@ -19,6 +19,31 @@ class Transaction(ApiBase):
     In case of simple purchases, the returned data will be mostly used for inspection.
 
     In complex transactions, opertaions on it are used to perform or cancel it.
+
+    ::
+        <transaction>
+            <reference_id>3dcFjTC7LDjIjTY3nkKjBVZ8qkZ</reference_id>
+            <transaction_token>53VFyQKYBmN9vKfA9mHCTs79L9a</transaction_token>
+            <created_at type="datetime">2011-04-22T17:57:56Z</created_at>
+            <descriptor>Custom descriptor here if your processor supports it.</descriptor>
+            <custom>Any value you like.</custom>
+            <transaction_type>purchase</transaction_type>
+            <amount>100.00</amount>
+            <currency_code>USD</currency_code>
+            <billing_reference>12345678</billing_reference>
+            <customer_reference>1234</customer_reference>
+            <processor_token>[Processor Token]</processor_token>
+            <processor_response>
+                <success type="boolean">false</success>
+                <messages>
+                <message class="error" context="processor.avs" key="country_not_supported" />
+                <message class="error" context="input.cvv" key="too_short" />
+                </messages>
+            </processor_response>
+            <payment_method>...</payment_method>
+        </transaction>
+
+    All elements inside root element `transaction` are directly available on the object.
     """
     top_xml_key = 'transaction'
 
@@ -66,6 +91,26 @@ class Transaction(ApiBase):
                     self.errors = message if isinstance(message, list) else [message]
                 return True
         return super(Transaction, self)._check_for_errors(parsed_res)
+
+    def is_success(self):
+        """
+        Returns True if the transaction succeeded.
+        """
+        if (getattr(self, 'processor_response') and self.processor_response.get('success')
+            and self.processor_response['success']):
+            return True
+        return False
+
+    def is_declined(self):
+        """
+        Returns True if the transaction is declined.
+        """
+        message_block = self._message_block
+        if message_block and message_block.get('message'):
+            messages = message_block['message']
+            return any(True for m in messages if isinstance(m, dict)
+                       and m.get('key') == 'decline')
+        return False
 
     def capture(self, amount):
         """

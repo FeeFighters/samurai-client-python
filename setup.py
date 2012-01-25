f6853884d938693bcb856e39e66113b2098132bf
Joshua Krall <joshuakrall@pobox.com>
2012-01-25 00:17:21 -0600

    Bumping to 0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name="samurai",
-    version="0.4",
+    version="0.5",
     description="Samurai Python API",
     author="samurai@feefighters.com",
     author_email="samurai@feefighters.com",
